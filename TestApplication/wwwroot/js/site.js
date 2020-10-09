2d0dab27acd7c7618f8e96c16b6d8c850d32b571
Rozario <albert.rozario.temp@brose.com>
2020-10-09 18:07:21 +0200

    Initial commit

---
+++ TestApplication/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    let location = (window.location.pathname + window.location.search).substr(1);
+    if (location === '') $('.d-menu').hide();
+    $('#myDropdown').click(function (e) {
+        $(this).children('.d-menu').toggle();
+        e.stopPropagation();
+
+    })
+});
